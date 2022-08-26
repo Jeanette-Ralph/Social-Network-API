838412f8619f15b2ed4e143468c18ab42098cf32
Jeanette Ralph <jean.ralph@hotmail.com>
2022-08-26 12:28:16 -0700

    added more files and worked on user model

---
+++ routes/api/friendsRoutes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+// for thoughts from friends list
+
+// POST route
+
+// DELETE route
+
+// for friends from friends list
+
+// POST route
+
+// DELETE route
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const usersRoutes = require('./courseRoutes');
 const thoughtsRoutes = require('./studentRoutes');
+const friendsRoutes = require('./friendstRoutes');
 
 router.use('/users', usersRoutes);
 router.use('/thoughts', thoughtsRoutes);
+router.use('/friends', friendsRoutes);
 
 module.exports = router;
